a1ff1364678734c74621c08509868dcc01f8380f
d-vooolk <volk.dmitrii@otr.ru>
2023-10-03 00:26:09 +0300

    fix last bags

--- src/components/SideBar/components/SavedMessages/index.jsx
+++ src/components/SideBar/components/SavedMessages/index.jsx
@@ -2,29 +2,29 @@ import './styles.scss';
 import {ReactComponent as EditIcon} from "../../../../static/edit.svg";
 import {ReactComponent as DeleteIcon} from "../../../../static/delete.svg";
 import {ReactComponent as ChatIcon} from "../../../../static/chat.svg";
-import {useContext, useState} from "react";
+import {useContext} from "react";
 import IncognitoInfoPanel from "./components/IncognitoInfoPanel";
 import {CurrentSettingsContext} from "../../../../context/UserSettingsContext";
 import {useDispatch, useSelector} from "react-redux";
-import {deleteChat, editChatTitle, setCurrentChat, terminateCurrentChat} from "../../../../redux/reducers/chatsSlice";
+import {deleteChat, editChatTitle, setCurrentChat} from "../../../../redux/reducers/chatsSlice";
 
 const SavedMessages = () => {
-    const [activeChat, setActiveChat] = useState(null);
     const {incognitoMode, setIncognitoMode} = useContext(CurrentSettingsContext);
     const dispatch = useDispatch();
 
     const savedMessages = useSelector(store => store.chatState.chats);
+    const currentChat = useSelector(state => state.chatState.currentChat);
+    const chatsLength = useSelector(state => state.chatState.chatsLength);
+
+    console.log('chatsLength', chatsLength);
 
     const toggleChatHandle = (id) => {
-        setActiveChat(id)
         dispatch(setCurrentChat({ id }))
     }
 
     const handleEditChatTitle = (id) => dispatch(editChatTitle({id, title: 'hello'}));
-    const handleDeleteChat = async (id) => {
-        await setActiveChat(null);
+    const handleDeleteChat = (id) => {
         dispatch(deleteChat({id}));
-        dispatch(terminateCurrentChat());
     }
 
     return (
@@ -37,16 +37,16 @@ const SavedMessages = () => {
             {
                 savedMessages.map((chat) => (
                     <div
-                        className={`${activeChat === chat.id ? 'saved-messages-wrapper-active' : 'saved-messages-wrapper-inactive'}`}
+                        className={`${currentChat === chat.id ? 'saved-messages-wrapper-active' : 'saved-messages-wrapper-inactive'}`}
                         onClick={() => toggleChatHandle(chat.id)}
                         key={`${chat.id + Math.random()}`}
                     >
                         <div
-                            className={`${activeChat === chat.id ? 'chat-text-wrapper-active' : 'chat-text-wrapper-inactive'}`}>
+                            className={`${currentChat === chat.id ? 'chat-text-wrapper-active' : 'chat-text-wrapper-inactive'}`}>
                             <ChatIcon className="chat-icon"/>
                             <span className="saved-messages-text">{chat.title}</span>
                         </div>
-                        {activeChat === chat.id && (
+                        {currentChat === chat.id && (
                             <div className="icons-wrapper">
                                 <EditIcon
                                     className="edit-icon"
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,8 @@ export const checkLocalStorage = (key, defaultValue) => {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : defaultValue;
 }
+
+export const getMaxUniqID = (chats) => {
+    const maxId = Math.max(...chats.map(chat => chat.id));
+    return maxId >= 0 ? maxId + 1 : 0;
+}
