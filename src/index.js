56f73b76440a16db8aefba3bcaa334d625c4e45b
d-vooolk <volk.dmitrii@otr.ru>
2023-12-27 13:29:01 +0300

    git learn 2

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     //comment for test
-    //new comment
-    //lknsldkvasv
+
     <BrowserRouter>
         <Provider store={store}>
             <App/>
